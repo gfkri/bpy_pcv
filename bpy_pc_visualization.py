89cdfd6f570fbd1bc13a41fd7b986f9ae55fe1ed
Georg Krispel <georg.krispel@icg.tugraz.at>
2025-01-07 17:04:19 +0100

    refactored

--- bpy_pc_visualization.py
+++ bpy_pc_visualization.py
@@ -13,7 +13,10 @@ import open3d as o3d
 import bpy
 from mathutils import Matrix, Vector
 
+from tools import frustum, utils
 
+
+#######################################################################################################################
 # Function to link assets from the library
 def load_assets(library_path, assets, link=False):
     # Open the library and import the specified assets
@@ -47,16 +50,126 @@ def apply_geometry_node_group(object, geometry_node_name):
     else:
         print(f"Geometry Node group '{geometry_node_name}' not found.")
     
-            
+    
 
+#######################################################################################################################
+def create_blender_camera(main_collection, camera_name, intrinsics, extrinsics, image_fp, img_dim, render_dim, 
+                          sensor_width, image_plane_scale, axis_scaling, frustum_material='Input Material'):
+    
+    
+    f_u, f_v, c_u, c_v = intrinsics['f_u'], intrinsics['f_v'], intrinsics['c_u'], intrinsics['c_v']
+        
+    # Camera matrix
+    # K = np.array([[f_u, 0, c_u], [0, f_v, c_v], [0, 0, 1]])
+    
+    # Distortion coefficients
+    dist_coeffs = np.array(intrinsics)
+
+    # Calculate the field of view
+    # fov_u = 2 * np.arctan(max_image_width / (2 * f_u))
+    # fov_v = 2 * np.arctan(max_image_height / (2 * f_v))
+    
+    max_image_width, max_image_height = render_dim
+    
+    # Focal length in m
+    focal_length = f_u / max_image_width * sensor_width
+    image_plane_distance = f_u / max_image_width * image_plane_scale
+    
+    R = extrinsics[:3, :3]
+    t = extrinsics[:3, 3:4]
+    
+    extrinsics = np.vstack((np.hstack((R, t)), np.array([0, 0, 0, 1])))
+    extrinsics = Matrix(extrinsics).to_4x4()
+            
+    # camera to vehicle ax swap: x>-y, y>z, z>-x to be consistent with blender
+    ax_swap = np.array([[ 0, 0, -1],
+                        [-1, 0,  0],
+                        [ 0, 1,  0]])
+    R = R @ ax_swap
+    T_cam2vehicle = np.vstack((np.hstack((R, t)), np.array([0, 0, 0, 1])))
+    T_cam2vehicle = Matrix(T_cam2vehicle).to_4x4()
+    
+    # create image plane
+    img_aspect_ratio = img_dim[1] / img_dim[0]
+
+    # image plane dimensions per default are: aspect ratio x 1.0
+    plane_height = 1.0
+    plane_width = img_aspect_ratio * plane_height
+    plane_scale = sensor_width / focal_length * image_plane_distance / plane_width
+
+    # Calculate offsets based on principal point
+    # TODO check if this is correct
+    offset_x = -(c_u - (img_dim[1] / 2)) * plane_width / img_dim[1]
+    offset_y = (c_v - (img_dim[0] / 2)) * plane_height / img_dim[0]
+
+    scale_matrix = Matrix.Scale(plane_scale, 4)
+    plane_distance_matrix = Matrix.Translation(Vector((0, 0, -image_plane_distance)))
+
+    # Create a translation matrix for the offset
+    principle_offset_matrix = Matrix.Translation(Vector((offset_x, offset_y, 0)))
+
+    # Combine the offset with the original camera-to-vehicle transformation
+    T_cam2vehicle_with_offset = T_cam2vehicle @ plane_distance_matrix @ scale_matrix @ principle_offset_matrix
+    
+    bpy.ops.image.import_as_mesh_planes(shader='SHADELESS', files=[{'name':image_fp}])
+    image_mesh = bpy.context.object
+    bpy.context.collection.objects.unlink(image_mesh)
+    main_collection.objects.link(image_mesh)
+    image_mesh.name = f'{camera_name}_IMAGE'
+    image_mesh.matrix_world = T_cam2vehicle_with_offset
+            
+    camera = bpy.data.cameras.new(name=camera_name)        
+    camera_object = bpy.data.objects.new(camera_name, camera)
+    main_collection.objects.link(camera_object)
+    # bpy.context.scene.collection.objects.unlink(camera_object)  
+
+    # Position the camera
+    # camera_object.location = tuple(t)  # Adjust as needed
+    camera.lens = focal_length * 1000  # Convert to mm as required by Blender
+    camera_object.matrix_world = T_cam2vehicle  # Adjust as needed
+    
+    # Create a new collection for the camera
+    frame_name = f'FRAME {camera_name}'
+    frame_collection = bpy.data.collections.new(frame_name)
+    # bpy.context.scene.collection.children.link(frame_collection)
+    main_collection.children.link(frame_collection)
+
+
+    if axis_scaling > 0:
+        vehicle_frame = bpy.data.objects.get("Vehicle Frame")
+        camera_frame = utils.duplicate_object(vehicle_frame, collection=frame_collection)
+        camera_frame.name = f'Frame'
+        camera_frame.matrix_world = extrinsics @ Matrix.Scale(axis_scaling, 4)
+
+    # Set the camera as the active camera
+    if camera_name == 'FRONT':
+        bpy.context.scene.camera = camera_object
+    else:
+        pass
+        # set inactive for rendering and visibility to false
+        # image_mesh.hide_render = True
+        # image_mesh.hide_viewport = True  
+        
+    frustum_obj = frustum.create_camera_frustum(camera_object, farplane_ratio=image_plane_scale, 
+                                                nearplane_ratio=0.1, thickness=0.01)
+    
+    # Apply the frustum material
+    frustum_obj.data.materials.append(bpy.data.materials[frustum_material])    
+    main_collection.objects.link(frustum_obj)
+    
+    
+            
+#######################################################################################################################
 def create_blender_scene():     
     
     library_path = "output/template.blend"
     
     
     # Names of the assets in the library
-    default_geometry_node = "Output Geometry"  # Name of the Geometry Nodes group
-    default_material = "Output Material"       # Name of the Material    
+    pc_geometry_node = "Output Geometry"  # Name of the Geometry Nodes group for the point clouds
+    pc_material = "Output Material"       # Name of the Material for the point clouds
+    frustum_material = "Input Material"   # Name of the Material for the frustums
+    
     # assets = {"materials": ['Output Material', 'Input Material'],
     #           "node_groups": ['Output Geometry', 'Input Geometry'],
     #           "worlds": ['World']}
@@ -72,7 +185,6 @@ def create_blender_scene():
     # cameras = []
     # lasers = []
 
-    
     input_dp = 'data/waymo/segment-10584247114982259878_490_000_510_000_with_camera_labels'
     # input_dp = 'data/waymo/segment-10455472356147194054_1560_000_1580_000_with_camera_labels'
     input_dp = 'data/waymo/segment-10837554759555844344_6525_000_6545_000_with_camera_labels'
@@ -82,8 +194,11 @@ def create_blender_scene():
     sensor_width = 0.036
 
     # in m
-    image_plane_distance = 100
-    combine_lasers = True
+    image_plane_scale = 0.4
+    combine_lasers = False
+    
+    # scaling <0 means no axis
+    axis_scaling = 0.4
     
     workspace = Path.cwd()
     output_dp = Path(output_dp)
@@ -105,6 +220,8 @@ def create_blender_scene():
         max_image_width = max(max_image_width, image.shape[1])
         max_image_height = max(max_image_height, image.shape[0])
         print('Image resolution:', image.shape)
+        
+    render_dim = (max_image_width, max_image_height)
     
     # Create a new Blender file
     bpy.ops.wm.read_factory_settings()       
@@ -118,16 +235,15 @@ def create_blender_scene():
     # bpy.data.scenes.remove(bpy.data.scenes[scene_name])
     # bpy.data.scenes[-1].name = scene_name   
     
-    collection = bpy.data.collections.get(frame_collection_name)
-    if collection:
+    main_collection = bpy.data.collections.get(frame_collection_name)
+    if main_collection:
         # Remove all objects in the collection
-        for obj in list(collection.objects):  # Use list() to avoid modification during iteration
+        for obj in list(main_collection.objects):  # Use list() to avoid modification during iteration
             bpy.data.objects.remove(obj, do_unlink=True)
         print(f"Cleared all objects in the collection: {frame_collection_name}")
     else:
         print(f"Collection '{collection_name}' not found.")
         
-    
     pcd_fps = [point_cloud_dp / laser_name / f"{frame_id:05d}_{laser_name}.ply" 
                for laser_name in lasers]
         
@@ -140,101 +256,39 @@ def create_blender_scene():
         o3d.io.write_point_cloud(str(comnbined_pcd_fp), pcd)
         bpy.ops.wm.ply_import(filepath=str(comnbined_pcd_fp))
         imported_object = bpy.context.view_layer.objects.active
-        apply_geometry_node_group(imported_object, default_geometry_node)
+        apply_geometry_node_group(imported_object, pc_geometry_node)
     else:
         for pcd_fp in pcd_fps:
             bpy.ops.wm.ply_import(filepath=str(pcd_fp))
             imported_object = bpy.context.view_layer.objects.active
-            apply_geometry_node_group(imported_object, default_geometry_node)
+            apply_geometry_node_group(imported_object, pc_geometry_node)
         
     for img_dim, camera_name in zip(image_dimensions, cameras):
-        
         # load camera calibration
         intrinsics_fp = calibration_dp / f"intrinsics_{camera_name}.npz"
-        intrinsics = np.load(intrinsics_fp, allow_pickle=True)        
-        f_u, f_v, c_u, c_v = intrinsics['f_u'], intrinsics['f_v'], intrinsics['c_u'], intrinsics['c_v']
-        
-        # Intrinsic matrix (3x3)
-        K = np.array([[f_u, 0, c_u], [0, f_v, c_v], [0, 0, 1]])
-        
-        # Distortion coefficients
-        dist_coeffs = np.array(intrinsics)
-
-        fov_u = 2 * np.arctan(max_image_width / (2 * f_u))
-        fov_v = 2 * np.arctan(max_image_height / (2 * f_v))
-        
-        # focal length in m
-        focal_length = f_u / max_image_width * sensor_width
-        
         extrinsics_fp = calibration_dp / f"extrinsics_{camera_name}.npy"
-        T_cam2vehicle = np.load(extrinsics_fp)
+        intrinsics = np.load(intrinsics_fp, allow_pickle=True)        
+        extrinsics = np.load(extrinsics_fp)
         
-        R = T_cam2vehicle[:3, :3]
-        t = T_cam2vehicle[:3, 3:4]
-                
-        # camera to vehicle ax swap: x>-y, y>z, z>-x
-        ax_swap = np.array([[ 0, 0, -1],
-                            [-1, 0,  0],
-                            [ 0, 1,  0]])
-        R = R @ ax_swap
-        T_cam2vehicle = np.vstack((np.hstack((R, t)), np.array([0, 0, 0, 1])))
-        T_cam2vehicle = Matrix(T_cam2vehicle).to_4x4()
-
-        # T_vehicle2cam = np.vstack((np.hstack((R.T, -R.T @ t)), np.array([0, 0, 0, 1])))
-        
-        # create image plane
-        img_aspect_ratio = img_dim[1] / img_dim[0]
-
-        # image plane dimensions per default are: aspect ratio x 1.0
-        plane_height = 1.0
-        plane_width = img_aspect_ratio * plane_height
-        plane_scale = sensor_width / focal_length * image_plane_distance / plane_width
-
-        # Calculate offsets based on principal point
-        # TODO check if this is correct
-        offset_x = -(c_u - (img_dim[1] / 2)) * plane_width / img_dim[1]
-        offset_y = (c_v - (img_dim[0] / 2)) * plane_height / img_dim[0]
-
-        scale_matrix = Matrix.Scale(plane_scale, 4)
-        plane_distance_matrix = Matrix.Translation(Vector((0, 0, -image_plane_distance)))
-
-        # Create a translation matrix for the offset
-        principle_offset_matrix = Matrix.Translation(Vector((offset_x, offset_y, 0)))
-
-        # Combine the offset with the original camera-to-vehicle transformation
-        T_cam2vehicle_with_offset = T_cam2vehicle @ plane_distance_matrix @ scale_matrix @ principle_offset_matrix
-
         image_fp = image_dp / camera_name / f"{frame_id:05d}_{camera_name}.jpg"       
         shutil.copy(image_fp, output_image_dp / image_fp.name)
-        bpy.ops.image.import_as_mesh_planes(shader='SHADELESS', files=[{'name':str(output_image_dp / image_fp.name)}])
-        image_mesh = bpy.context.object
-        image_mesh.name = f'{camera_name}_IMAGE'
-        image_mesh.matrix_world = T_cam2vehicle_with_offset
-               
-        camera = bpy.data.cameras.new(name=camera_name)
-        camera_object = bpy.data.objects.new(camera_name, camera)
-        bpy.context.collection.objects.link(camera_object)
+        
+        camera_collection = bpy.data.collections.new(f'Camera {camera_name}')
+        # bpy.context.scene.collection.children.link(camera_collection)
+        main_collection.children.link(camera_collection)
+        
+        create_blender_camera(camera_collection, camera_name, intrinsics, extrinsics, 
+                              str(output_image_dp / image_fp.name), img_dim, render_dim, 
+                              sensor_width, image_plane_scale, axis_scaling, frustum_material)
 
-        # Position the camera
-        # camera_object.location = tuple(t)  # Adjust as needed
-        camera.lens = focal_length * 1000  # Convert to mm as required by Blender
-        camera_object.matrix_world = T_cam2vehicle  # Adjust as needed
-
-        # Set the camera as the active camera
-        if camera_name == 'FRONT':
-            bpy.context.scene.camera = camera_object
-        else:
-            # set inactive for rendering and visibility to false
-            image_mesh.hide_render = True
-            image_mesh.hide_viewport = True  
-            
-            
     # Set the resolution
     bpy.context.scene.render.resolution_x = max_image_width
     bpy.context.scene.render.resolution_y = max_image_height
         
-    output_path = output_dp / "highway_output_file.blend"  # Update with your desired output path
+    output_path = output_dp / "output_file.blend"  # Update with your desired output path
     bpy.ops.wm.save_as_mainfile(filepath=str(output_path))
+    
+
         
     print('Done')
            
